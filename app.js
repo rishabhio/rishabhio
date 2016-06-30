ed341d3d07d484264f3013974ce82f0fbcdcd767
rishabhio <rishabh@inoeve.com>
2016-06-30 15:46:03 +0530

    added remaining views and controlers

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/home',
             templateUrl: 'partial-home.html'
         })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('about', {
-            // we'll get to this in a bit       
-        })
-		
 		    // nested list with custom controller
     .state('home.list', {
         url: '/list',
@@ -31,5 +25,49 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         url: '/paragraph',
         template: 'I could sure use a drink right now.'
     })
+	
+	 .state('about', {
+        url: '/about',
+        views: {
+
+            // the main template will be placed here (relatively named)
+            '': { templateUrl: 'partial-about.html' },
+
+            // the child views will be defined here (absolutely named)
+            'columnOne@about': { template: 'Look I am a column!' },
+
+            // for column two, we'll define a separate controller 
+            'columnTwo@about': { 
+                templateUrl: 'table-data.html',
+                controller: 'scotchController'
+            }
+        }
         
+    });
+
+}); // closes $routerApp.config()
+        
+		routerApp.controller('scotchController', function($scope) {
+    
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+    
+});
+		
+		
+		
 });
