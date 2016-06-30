a9b13ffb352c689abd433d90312b67aafca2f9a9
rishabhio <rishabh@inoeve.com>
2016-06-30 15:36:24 +0530

    adding nested views

--- app.js
+++ app.js
@@ -16,5 +16,20 @@ routerApp.config(function($stateProvider, $urlRouterProvider) {
         .state('about', {
             // we'll get to this in a bit       
         });
+		
+		    // nested list with custom controller
+    .state('home.list', {
+        url: '/list',
+        templateUrl: 'partial-home-list.html',
+        controller: function($scope) {
+            $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+        }
+    })
+
+    // nested list with just some random string data
+    .state('home.paragraph', {
+        url: '/paragraph',
+        template: 'I could sure use a drink right now.'
+    })
         
 });
